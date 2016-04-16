bfb5e18342f4e976217f5a203c76f88bee92f1cd
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-04-16 20:22:55 +0200

    Monster stuff

---
+++ boxingAR/Assets/Scripts/Monster.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject ProjectilePrefab;
+    public float ProjectileDelay=2;
+    private float _currentTime=0;
+    public bool Active;
+    public float TotalHealth;
+    private float _health;
+    private Texture2D texture;
+
+	// Use this for initialization
+	void Start ()
+	{
+        texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, Color.red);
+        texture.wrapMode = TextureWrapMode.Repeat;
+        texture.Apply();
+	    _health = TotalHealth;
+	}
+
+    private void LaunchProjectile()
+    {
+        GameObject go = (GameObject)Instantiate(ProjectilePrefab,transform.position,transform.rotation);
+        Projectile pr = go.GetComponent<Projectile>();
+        if (pr != null)
+        {
+            pr.Initialize((Camera.main.transform.position-transform.position).normalized);
+        }
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+        if (Active)
+        {
+            _currentTime += Time.deltaTime;
+
+            if (_currentTime > ProjectileDelay)
+            {
+                _currentTime = 0;
+                LaunchProjectile();
+            } 
+        }
+	}
+
+    public void Damage(float amount)
+    {
+        _health -= amount;
+        if (amount <= 0)
+        {
+            //Todo: Notify the main class upon death
+            Destroy(gameObject);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (Active)
+        {
+            GUI.DrawTexture(new Rect(0,0,Screen.width*(_health/TotalHealth),Screen.height/10f), texture);
+        }
+    }
+}
--- boxingAR/Assets/Scripts/Projectile.cs
+++ boxingAR/Assets/Scripts/Projectile.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
+    public float LifeSpan=2;
+    public float Speed=1;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
 	}
+
+    //Todo: Add reference to main object to increase timer on collision
+    public void Initialize(Vector3 direction)
+    {
+        Rigidbody rigid = GetComponent<Rigidbody>();
+        if (rigid!=null)
+        {
+            rigid.velocity =Speed*direction;
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+	    LifeSpan -= Time.deltaTime;
+	    if (LifeSpan <= 0)
+	    {
+	        Destroy(gameObject);
+	    }
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Todo: Increase time
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
 }
