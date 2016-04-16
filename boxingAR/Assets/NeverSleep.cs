db0b8a0c19e2640d6c7a12f7484827b2a766c468
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-04-16 16:39:22 +0200

    Made changes necessary for google glass

---
+++ boxingAR/Assets/NeverSleep.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeverSleep : MonoBehaviour {
+
+    //Place this on some script in the scene to prevent google glass from breaking everything after a while
+
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
