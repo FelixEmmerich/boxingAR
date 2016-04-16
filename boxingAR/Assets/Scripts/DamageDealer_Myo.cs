df658e2ea1560ca3b6fd37cbb61e49d5459868ba
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-04-16 22:11:04 +0200

    Damage!

---
+++ boxingAR/Assets/Scripts/DamageDealer_Myo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Thalmic.Myo;
+
+public class DamageDealer_Myo : DamageDealer
+{
+    public ThalmicMyo Myo;
+    public float DamageScale=1;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("abc");
+        Monster monster = collision.gameObject.GetComponent<Monster>();
+        if (monster != null)
+        {
+            Debug.Log(Myo.accelerometer.magnitude);
+            Damage(monster, Myo.accelerometer.magnitude*DamageScale);
+        }
+    }
+
+}
