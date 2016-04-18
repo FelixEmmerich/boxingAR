6bd337cd37381b7f0ce13a72b51beb6d9e7c3a87
FelixEmmerich <Felix-Emmerich@Freenet.de>
2016-04-18 10:17:58 +0200

    Highscore system

--- boxingAR/Assets/Scripts/Scores.cs
+++ boxingAR/Assets/Scripts/Scores.cs
@@ -1,31 +1,60 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 
-public class LoadScores : MonoBehaviour {
+public class LoadScores : MonoBehaviour
+{
+	static string filestring = Application.persistentDataPath+@"/highscore.boxing";
 
-	/*//string filestring = @SavePath + "/" + CustomDirectory + "/"; 
+    [System.Serializable]
+    public class HighscoreData:IComparable<HighscoreData>
+    {
+        public string Name;
+        public float Time;
+        public HighscoreData(string name, float time)
+        {
+            Name = name;
+            Time = time;
+        }
+        public int CompareTo(HighscoreData other)
+        {
+            return Time.CompareTo(other.Time);
+        }
+    }
 
+    public static int AddToHighscore(string name, float time)
+    {
+        List<HighscoreData> list = LoadHighscore();
+        int position=AddHighscoreDataToList(new HighscoreData(name, time), ref list);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(filestring, FileMode.OpenOrCreate);
+        bf.Serialize(file, list);
+        file.Close();
+        return position;
+    }
 
-	// Use this for initialization
-	void Start ()
-	{
-		float[] scores;
-		//StreamReader inp = new StreamReader (filestring);
+    public static List<HighscoreData> LoadHighscore()
+    {
+        if (File.Exists(filestring))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(filestring, FileMode.Open);
+            List<HighscoreData>list = (List<HighscoreData>)bf.Deserialize(file);
+            file.Close();
+            return list;
+        }
+        Debug.Log("File can't be loaded: " + filestring + " doesn't exist");
+        return new List<HighscoreData>();
+    }
 
-		while (!inp.EndOfStream)
-		{
-			float inp_ln = inp.ReadLine ();
-			scores += inp_ln;
-		}
-		inp.Close ();
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}*/
+    public static int AddHighscoreDataToList(HighscoreData hsd, ref List<HighscoreData> list)
+    {
+        list.Add(hsd);
+        list.Sort();
+        return list.IndexOf(hsd);
+    }
 }
 
